6675a692801d5116c53ec12e8aa2c0a9b25f2233
王键 <wb-wj449816@antfin.com>
2018-11-22 17:48:38 +0800

    有趣的屏蔽工具

--- src/main/java/com/frank/biz/Test.java
+++ src/main/java/com/frank/biz/Test.java
@@ -5,7 +5,7 @@ import com.frank.biz.info.UserCommonInfo;
 public class Test {
 
     public static void main(String[] args) {
-        UserCommonInfo user = new UserCommonInfo("王键","18684031132","510107199606152617");
+        UserCommonInfo user = new UserCommonInfo("王键","18684523345","510107200010012617");
         System.out.println(user.toString());
     }
 }
