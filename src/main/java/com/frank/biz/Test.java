97fa439540d8a8f55c9103f8068b7479a2c43665
Administrator <1009057923@qq.com>
2018-12-13 21:20:00 +0800

    Guava中的校验方法

--- src/main/java/com/frank/biz/Test.java
+++ src/main/java/com/frank/biz/Test.java
@@ -1,12 +1,12 @@
 package com.frank.biz;
 
-import com.frank.biz.info.UserCommonInfo;
+import com.frank.biz.shields.AllShield;
 
 public class Test {
 
     public static void main(String[] args) {
-        UserCommonInfo user = new UserCommonInfo("王键", "18684523345", "510107200010012617");
-        System.out.println(user.toString());
+        AllShield abstractShield = new AllShield();
+        System.out.println(abstractShield.toString());
     }
 
 }
