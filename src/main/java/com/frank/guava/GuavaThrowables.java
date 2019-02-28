487fadb1f42566fb148837dc359b82ede4a80259
王键 <wb-wj449816@antfin.com>
2019-02-28 17:59:44 +0800

    简化异常处理的Throwables类

--- src/main/java/com/frank/guava/GuavaThrowables.java
+++ src/main/java/com/frank/guava/GuavaThrowables.java
@@ -30,20 +30,6 @@ import java.net.URL;
 public class GuavaThrowables {
 
     /**
-     * 传递异常的常用方法：
-     *
-     * 　　1.RuntimeException propagate(Throwable)：把throwable包装成RuntimeException，用该方法保证异常传递，
-     *      抛出一个RuntimeException异常
-     * 　　2.void propagateIfInstanceOf(Throwable, Class<X extends Exception>) throws X：
-     *      当且仅当它是一个X的实例时，传递throwable
-     * 　　3.void propagateIfPossible(Throwable)：当且仅当它是一个RuntimeException和Error时，传递throwable
-     * 　　4.void propagateIfPossible(Throwable, Class<X extends Throwable>) throws X：
-     *      当且仅当它是一个RuntimeException和Error时，或者是一个X的实例时，传递throwable。
-     */
-
-
-
-    /**
      * 有时候, 当我们我们捕获异常, 并且像把这个异常传递到下一个try/catch块中。
      * Guava提供了一个异常处理工具类, 可以简单地捕获和重新抛出多个异常。例如：
      */
